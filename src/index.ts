db14d4178d7b381ceb44eeea7a8e9bbf3cdd8602
Nwakasi Stephen <nwakasistephen@gmail.com>
2024-05-15 23:29:17 +0100

    chore(dotenv):setup env management

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(7000, () => {
-  console.log("Server is now running at port " + 7000);
+app.listen(process.env.PORT, () => {
+  console.log("Server is now running at port " + process.env.PORT);
 });
